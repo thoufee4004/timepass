a995a71e9adc886825b9601e26bc19e795cd9841
B.Al mohammed thoufeek <42872880+thoufee4004@users.noreply.github.com>
2019-02-19 12:05:02 +0530

    Create taxi2.py

---
+++ taxi2.py
@@ -0,0 +1,25 @@
+try:
+	b=raw_input()
+	a=0
+	if (b.isalpha()):
+	  print('wrong input')
+	elif not b:
+	  print('enter anything')
+	elif float(b)<=0:
+	  print("wrong destination")
+	else:
+	            km=a+float(b)
+	            print ('total km=',km)
+	            print("1.micro\n 2.mini\n 3.premium\n")
+	            n=float(raw_input())
+	            key=round(n)
+	            if float(key)==1:
+	              print("fare=",km*10)
+	            elif float(key)==2:
+	              print("fare=",km*15)
+	            elif float(key)==3:
+	              print("fare=",km*30)
+	            else:
+	              print("invalid choice")
+except ValueError:
+	print("false input")
